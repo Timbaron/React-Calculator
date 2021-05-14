035aa208aca295ae65b4d5085fefb9af3df9f630
Akiode Timothy <akiodetimothy2017@gmail.com>
2021-05-14 07:41:27 +0100

    Create Operation and Clear buttons

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import './App.css';
 
 export function App() {
   const numbers = [1,2,3,4,5,6,7,8,9];
+  const operations = ["+","-","*","/"];
   return (
     <div className="App">
-      <div className="numbers">
-
-      {numbers.map((val,key) => {
-        return <div className="individualNumber">{val}</div>
-      })}
+      <div className="calculator">
+        <div className="display"></div>
+          <div className="buttons">
+            <div className="leftSide">
+              <div id="clearBtn">Clear</div>
+                <div className="numbers">
+                  {numbers.map((val,key) => {
+                    return <div id="individualNumber">{val}</div>
+                  })}
+                </div>
+              </div>
+              <div className="leftSide">
+                {operations.map((val,key) => {
+                  return <div id="operations">{val}</div>
+                } )}
+          </div>
+        </div>
       </div>
     </div>
   )
